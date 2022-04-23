4e581966cd45b623ae603aa683f27e81a912e53c
leeheezin <102892982+leeheezin@users.noreply.github.com>
2022-04-23 22:33:19 +0900

    Add files via upload

--- music player/js/music-list.js
+++ music player/js/music-list.js
@@ -1,36 +1,36 @@
 let allMusic = [
   {
-    name: "BIGBANG - 봄여름가을겨울",
+    name: "봄여름가을겨울",
     artist: "BIGBANG",
     img: "music1",
     src: "music1"
   },
   {
-    name: "임영웅 - 사랑은 늘 도망가",
-    artist: "임영웅",
+    name: "Perfect for Me",
+    artist: "Jay Ollero",
     img: "music2",
     src: "music2"
   },
   {
-    name: "멜로망스 - 취중고백",
+    name: "취중고백",
     artist: "멜로망스",
     img: "music3",
     src: "music3"
   },
   {
-    name: "박재범 - 가나다라마바사",
+    name: "가나다라마바사",
     artist: "박재범",
     img: "music4",
     src: "music4"
   },
   {
-    name: "원슈타인 - 존재만으로",
+    name: "존재만으로",
     artist: "원슈타인",
     img: "music5",
     src: "music5"
   },
   {
-    name: "justin bibber - stay",
+    name: "stay",
     artist: "justin bibber",
     img: "music6",
     src: "music6"
