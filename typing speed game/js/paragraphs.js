229943b65b1d0fc2b6d764155db21069cdb3555b
leeheezin <102892982+leeheezin@users.noreply.github.com>
2022-04-26 02:30:35 +0900

    Delete typing speed game directory

--- typing speed game/js/paragraphs.js
+++
@@ -1,8 +0,0 @@
-const paragraphs = [
-  "Miracles happen to only those who believe",
-  "Think like a man of action and act like man of thought.",
-  "Courage is very important. Like a muscle, it is strengthened by use.Miracles happen to only those who believe in them.",
-  "By doubting we come at the truth.",
-  "A man that has no virtue in himself, ever envies virtue in others.",
-
-];
